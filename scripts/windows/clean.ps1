a95e907697dcf32f6c4afbbc1272187ca6aecce5
Artic42 <main@artic42.com>
2024-08-13 10:24:32 +0200

    See character number 1 #28

---
+++ scripts/windows/clean.ps1
@@ -0,0 +1,5 @@
+docker stop casinoRoyale
+docker rm casinoRoyale
+docker rmi casino_royale
+remove-item .\poetry.lock
+remove-item .\test.db
---
+++ web/js/character.js
@@ -0,0 +1,27 @@
+var hostname = window.location.hostname;
+var playerID = 1;
+
+function readCharacter() {
+    const characterID = 1;
+
+    // Fetch character data from server api
+    fetch(`http://${hostname}:8000/character/${playerID}`)
+    .then(response => response.json())
+    .then(json => {
+        // Display character data
+        console.log(json);
+        console.log(json.Name);
+        document.getElementById('name').innerText = json.Name;
+        document.getElementById('healthValue').innerText = json.Health;
+        document.getElementById('strengthValue').innerText = json.Strength;
+        document.getElementById('hackValue').innerText = json.Hacking;
+        document.getElementById('medicineValue').innerText = json.Medicine;
+    });
+}
+   
+
+// Run readCharacter immediately
+readCharacter();
+
+// Set interval to run readCharacter every 30 seconds (30000 milliseconds)
+setInterval(readCharacter, 30000);
