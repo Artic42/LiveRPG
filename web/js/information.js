832b39ed709888bf629ae976cfa461bd2f7efe2b
Iñaki Arrondo <main@artic42.com>
2024-08-14 12:09:53 +0200

    Information for ID 1 #30

--- web/js/information.js
+++ web/js/information.js
@@ -2,24 +2,63 @@ const hostname = window.location.hostname;
 const playerID = 1;
 
 // Global variables for wait function
+var presentID = 0;
 
-function getOptions() {
-    fetch(`http://${hostname}:8000/information/readKnownCharacters/${playerID}`) 
+
+async function getOptions() {
+    const selectElement = document.getElementById('informationDropdown');
+    const response = await fetch(`http://${hostname}:8000/information/readKnownCharacters/${playerID}`);
+    const json = await response.json();
+    console.log(json);
+    const IDs = json.knownIDs;
+    console.log(IDs);
+    for (const ID of IDs) {
+        console.log(ID);
+        const name = await getNameAbout(ID);
+        console.log(name);
+        const newOption = document.createElement('option');
+        newOption.value = ID;
+        newOption.innerText = name; 
+        selectElement.appendChild(newOption);           
+    }
+}
+
+async function getNameAbout(ID) {
+    const response = await fetch(`http://${hostname}:8000/information/readAboutName/${ID}`);
+    const json = await response.json();
+    console.log(json);
+    console.log(json.name);
+    return json.name;
+}
+
+function readDescription() {
+    const descriptionElement = document.getElementById('description');
+    if (presentID == 0) {
+        descriptionElement.innerText = '';
+        return;
+    }
+    fetch(`http://${hostname}:8000/information/readDescription/${presentID}`)
     .then(response => response.json())
     .then(json => {
         console.log(json);
-        const IDs = json.KnownIDs;
-
-        for (const ID in IDs) {
-            
-        }
+        descriptionElement.innerText = json.description;
     });
 }
 
-function getNameAbout(ID) {
-    fetch(`http://${hostname}:8000/information/readAboutName/${ID}`)
-    .then(response => response.json())
-    .then(json => {
-        console.log(json);
-    });
+function cleanSelect() {
+    const selectElement = document.getElementById('informationDropdown');
+    selectElement.innerHTML = '';
 }
+
+function checkCharacterChange() {
+    const newID = document.getElementById('informationDropdown').value;
+    if (newID != presentID) {
+        presentID = newID;
+        readDescription();
+    }
+}
+
+// Call the function to populate the select element
+readDescription();
+getOptions();
+setInterval(checkCharacterChange, 1000);
