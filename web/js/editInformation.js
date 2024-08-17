75cefb9be5fa3e0a358813a35f7892a172e5d200
Iñaki Arrondo <main@artic42.com>
2024-08-17 11:47:35 +0200

    Information edit page fully functional #37

---
+++ web/js/editInformation.js
@@ -0,0 +1,48 @@
+const hostname = window.location.hostname;
+var informationID = 0;
+
+function sendInformationChange() {
+    // Get form values
+    const known = document.getElementById('knownID').value;
+    const about = document.getElementById('aboutID').value;
+    const description = document.getElementById('descriptionTextInput').value;
+
+    // Create a JSON object with the form values
+    const informationData = {
+        knownCharacter: known,
+        aboutCharacter: about,
+        description: description
+    };
+
+    console.log(informationData);
+
+    fetch(`http://${hostname}:8000/information/editFull/${informationID}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(informationData),
+    })
+    .then(response => response.json())
+    .then(json => console.log(json))
+}
+
+function readInformation() {
+    informationID = document.getElementById('informationID').value;
+    document.getElementById('InformationIDNumber').innerText = "ID: " + informationID;
+
+    fetch(`http://${hostname}:8000/information/readFull/${informationID}`)
+    .then(response => response.json())
+    .then(json => {
+        // Display information data
+        console.log(json);
+        document.getElementById('knownID').value = json.knownCharacter;
+        document.getElementById('aboutID').value = json.aboutCharacter;
+        document.getElementById('descriptionTextInput').value = json.description;
+    });
+}
+
+
+// Add event listener to the buttons
+document.getElementById("buttonSave").addEventListener("click", sendInformationChange);
+document.getElementById("buttonLoad").addEventListener("click", readInformation);
